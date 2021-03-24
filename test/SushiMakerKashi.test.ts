6cdb43ba36c3cec3f9cb4fb556c991ff84dd03ad
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-03-24 19:21:27 -0400

    Update SushiMakerKashi.test.ts
    test

--- test/SushiMakerKashi.test.ts
+++ test/SushiMakerKashi.test.ts
@@ -46,8 +46,8 @@ describe("KashiSushiMaker", function () {
     await this.bento.deposit(this.weth.address, this.alice.address, this.alice.address, getBigNumber(10), 0)
     await this.bento.deposit(this.strudel.address, this.alice.address, this.alice.address, getBigNumber(10), 0)
     await this.bento.setMasterContractApproval(this.alice.address, this.kashiMaster.address, true, "0", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000")
-    //const initData = defaultAbiCoder.encode(["address", "address", "address", "bytes"], [this.sushi.address, this.dai.address, this.oracle.address, oracleData])
-    //await this.bento.deploy(this.KashiMaster, initData, true)
+    const initData = defaultAbiCoder.encode(["address", "address", "address", "bytes"], [this.sushi.address, this.dai.address, this.oracle.address, this.oracle.getDataParameter(1)])
+    await this.bento.deploy(this.KashiMaster, initData, true)
   })
 
   describe("setBridge", function () {
