9b348e936445b3fa923bf29023e18a5eaff45734
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-03-25 11:47:13 -0400

    Update SushiMakerKashi.test.ts

--- test/SushiMakerKashi.test.ts
+++ test/SushiMakerKashi.test.ts
@@ -46,7 +46,7 @@ describe("KashiSushiMaker", function () {
     await this.bento.deposit(this.weth.address, this.alice.address, this.alice.address, getBigNumber(10), 0)
     await this.bento.deposit(this.strudel.address, this.alice.address, this.alice.address, getBigNumber(10), 0)
     await this.bento.setMasterContractApproval(this.alice.address, this.kashiMaster.address, true, "0", "0x0000000000000000000000000000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000000000000000000000000000000")
-    const oracleData = await this.oracle.getDataParameter()
+    const oracleData = await this.oracle.getDataParameter("0")
     const initData = defaultAbiCoder.encode(["address", "address", "address", "bytes"], [this.sushi.address, this.dai.address, this.oracle.address, oracleData])
     await this.bento.deploy(this.KashiMaster.address, initData, true)
   })
